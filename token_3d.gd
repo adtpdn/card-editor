08afa4bae0aae56af9e488ec303c3ad14c10625c
adtpdn <adtpdn@gmail.com>
2025-01-21 17:19:39 +0800

    working element token

--- token_3d.gd
+++ token_3d.gd
@@ -1,14 +1,21 @@
 extends Node3D
 
 enum BiomeType {FOREST, DESERT, MOUNTAIN, WATER}
-enum TokenType {TYPE1, TYPE2, TYPE3}
+enum TokenType {TRIANGLE, SQUARE, CIRCLE}
 
 var biome_type: BiomeType
 var token_type: TokenType
 @onready var outline_mesh: MeshInstance3D = $OutlineMesh  # Outer ring mesh
 @onready var token_mesh: MeshInstance3D = $TokenMesh  # Inner token mesh
+@onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
 var owner_id: int = -1
+
+const TYPE_MESHES = {
+	TokenType.TRIANGLE: preload("res://assets/meshes/triangle_mesh.tres"),
+	TokenType.SQUARE: preload("res://assets/meshes/rectangle_mesh.tres"),
+	TokenType.CIRCLE: preload("res://assets/meshes/cylinder_mesh.tres")
+}
 
 const BIOME_COLORS = {
 	BiomeType.FOREST: Color(0.2, 0.8, 0.2),  # Green
@@ -22,6 +29,15 @@ func set_token_data(b_type: BiomeType, t_type: TokenType, p_id: int = -1):
 	biome_type = b_type
 	token_type = t_type
 	owner_id = p_id
+	
+	# Update mesh based on type
+	if TYPE_MESHES.has(token_type):
+		mesh_instance.mesh = TYPE_MESHES[token_type]
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color(0, 0, 0)
+	mesh_instance.material_override = material
+	
 	update_token_display()
 
 func update_token_display():
