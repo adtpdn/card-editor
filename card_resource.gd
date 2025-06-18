a85f49cd091f4e3c5ac1afdf75373b7c870dec0a
Yogi Wiguna <yogiwigunand@gmail.com>
2025-06-18 12:33:39 +0800

    update card data

--- card_resource.gd
+++ card_resource.gd
@@ -3,6 +3,7 @@ class_name CardResource
 
 enum CardType {ACTION, AREA}
 
+@export var card_id : int = -1
 @export var card_name: String = "New Card"
 @export var card_type: CardType = CardType.ACTION
 @export var cost_to_draw: int = 1
@@ -12,7 +13,8 @@ enum CardType {ACTION, AREA}
 @export var tex3D_path: String = ""
 @export var image_path: String = ""
 
-func _init(p_name = "New Card", p_type = CardType.ACTION, p_cost = 1, p_effect1 = "", p_effect2 = ""):
+func _init(p_id = -1, p_name = "New Card", p_type = CardType.ACTION, p_cost = 1, p_effect1 = "", p_effect2 = ""):
+	card_id = p_id
 	card_name = p_name
 	card_type = p_type
 	cost_to_draw = p_cost
@@ -21,6 +23,7 @@ func _init(p_name = "New Card", p_type = CardType.ACTION, p_cost = 1, p_effect1 
 
 func to_dictionary() -> Dictionary:
 	return {
+		"card_id" : card_id,
 		"card_name": card_name,
 		"card_type": card_type,
 		"cost_to_draw": cost_to_draw,
@@ -32,6 +35,7 @@ func to_dictionary() -> Dictionary:
 	}
 
 func from_dictionary(data: Dictionary) -> void:
+	card_id = data["card_id"]
 	card_name = data["card_name"]
 	card_type = data["card_type"]
 	cost_to_draw = data["cost_to_draw"]
